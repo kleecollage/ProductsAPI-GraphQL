113c6fa33f52d55db85ea6f1914c48f3aa655f54
Klee Collage <klee.collage@gmail.com>
2024-12-11 13:21:14 -0600

    chore(config): Setup db connection and server

--- index.ts
+++ index.ts
@@ -1,12 +1,18 @@
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
+import { sequelize } from './config/database';
 import { ProductRouter } from './routes/products.route';
 
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use('/products', ProductRouter);
-app.listen( process.env.PORT, () => {
+app.listen( process.env.PORT, async () => {
   console.log('App listening on port: ', process.env.PORT);
+  try {
+    await sequelize.authenticate();
+  } catch (error) {
+    console.log(error)
+  }
 });

f1d15a7e223d24538aec9254af53d5ecb9b06539
Klee Collage <klee.collage@gmail.com>
2024-12-11 14:55:30 -0600

    fix(controller): Add type Promise<void> to endpoints

--- config/database.ts
+++ config/database.ts
@@ -4,7 +4,7 @@ import { Sequelize } from "sequelize";
 dotenv.config();
 
 export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: 'localhost',
+  host: 'db',
   dialect: 'mysql',
   port: parseInt(process.env.DB_PORT)
 });
--- controllers/product.controller.ts
+++ controllers/product.controller.ts
@@ -1,59 +1,63 @@
-import { Request, Response } from "express"
-import Product from "../models/products.model"
+import { NextFunction, Request, Response } from "express";
+import Product from "../models/products.model";
 
 //** ------------------------------ GET ALL PRODUCTS ------------------------------  **//
-export const getAllProducts = async (req: Request, res: Response) => {
-  const products = await Product.findAll()
-  return res.status(200).json({
-    products
-    // msg: 'Products ok',
-  })
+export const getAllProducts = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  try {
+    const products = await Product.findAll()
+    res.status(200).json({
+      products,
+      msg: 'Products ok',
+    });
+  } catch (error) {
+    next(error)
+  }
 }
 //** ------------------------------ GET PRODUCT BY ID ------------------------------  **//
-export const getProductById = async (req: Request, res: Response) => {
+export const getProductById = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   const { id } = req.params
+  try {
+    if (!id) res.status(404).json({ msg: 'ID not exists'})
 
-  if (!id) return res.status(404).json({ msg: 'ID not exists'})
+    const product = await Product.findOne({
+      where: {
+        id,
+        active: true
+      },
+    });
 
-  const product = await Product.findOne({
-    where: {
-      id,
-      active: true
-    },
-  });
+    if (!product) res.status(404).json({ msg: 'Product not found'})
 
-  if (!product) return res.status(404).json({ msg: 'Product not found'})
+      res.status(200).json({
+      product,
+      msg: 'Products ok',
+    })
+  } catch (error) {
+    next(error)
+  }
 
-  return res.status(200).json({
-    product
-    // msg: 'Products ok',
-  })
 }
 //** ------------------------------ CREATE PRODUCT ------------------------------  **//
-export const createProduct = async (req: Request, res: Response) => {
+export const createProduct = async (req: Request, res: Response): Promise<void> => {
   const {name, price, stock} = req.body
 
   try {
-    const product = await Product.create({
-      name,
-      price,
-      stock
-    })
+    const product = await Product.create({name, price, stock})
 
-    return res.status(200).json({ product })
+    res.status(200).json({ product })
   } catch (error) {
-    return res.status(500).json({
+    res.status(500).json({
       msg: `Product ${name} is not created`,
       error,
     })
   }
 }
 //** ------------------------------ UPDATE PRODUCT ------------------------------  **//
-export const updateProduct = async (req: Request, res: Response) => {
+export const updateProduct = async (req: Request, res: Response): Promise<void> => {
   const {name, price, stock} = req.body;
   const { id } = req.params;
 
-  if (!id) return res.status(404).json({ msg: 'ID not exists'})
+  if (!id) res.status(404).json({ msg: 'ID not exists'})
 
   try {
     const product = await Product.update({
@@ -68,19 +72,19 @@ export const updateProduct = async (req: Request, res: Response) => {
     },
   )
 
-    return res.status(200).json({ product })
+   res.status(200).json({ product })
   } catch (error) {
-    return res.status(500).json({
+   res.status(500).json({
       msg: `Product ${name} is not created`,
       error,
     })
   }
 }
 //** ------------------------------ DELETE PRODUCT ------------------------------  **//
-export const deleteProduct = async (req: Request, res: Response) => {
+export const deleteProduct = async (req: Request, res: Response): Promise<void> => {
   const { id } = req.params;
 
-  if (!id) return res.status(404).json({ msg: 'ID not exists'})
+  if (!id) res.status(404).json({ msg: 'ID not exists'})
 
   try {
     const product = await Product.update({
@@ -93,9 +97,9 @@ export const deleteProduct = async (req: Request, res: Response) => {
     },
   )
 
-    return res.status(200).json({ product })
+   res.status(200).json({ product })
   } catch (error) {
-    return res.status(500).json({
+   res.status(500).json({
       msg: `Product ${id} not exists`,
       error,
     })
